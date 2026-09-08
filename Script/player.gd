a7ce28e19a40869a7ea304453176cbb74603f000
Smurf <nosos.demo.concluido.27@gmail.com>
2026-09-08 14:20:28 -0300

    Envio
    Já na versão 0.1.5.

--- Script/player.gd
+++ Script/player.gd
@@ -9,7 +9,7 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	if Input.is_action_just_pressed("W") or Input.is_action_just_pressed("Space") and is_on_floor():
+	if Input.is_action_just_pressed("W") and is_on_floor() or Input.is_action_just_pressed("Space") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	var direction := Input.get_axis("A", "D")
